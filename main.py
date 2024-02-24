0bcd7ad59b670598d8d948f1163974ef993f3eca
Roman <123@gmail.com>
2024-02-24 11:41:24 +0200

    exam ver. 1.1

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ for page in range(1, 7):
     response = session.get(url, headers=header)
     soup = BeautifulSoup(response.text, "lxml")
 
-    all_shop = soup.find_all("div", class_="shop-title")
-    # all_shop_cashback = soup.find_all("div", class_="shop-rate")
+    all_shop = soup.find("div", class_="row col-lg-9 col-md-9 col-12")
+    shop = all_shop.find_all("div", class_="col-lg-2 col-md-3 shop-list-card pseudo-link no-link")
+    shop_body = shop.find_all("div", class_="card-body")
 
     for elem in all_shop:
-        logo_text = elem.find("p", class_="shop-text").text
+        logo_text = elem.find("div", class_="shop-title").text
         logo_text1 = logo_text[logo_text.find(":")+1:logo_text.find("%")]
-        cashback_text = elem.find("p", class_="shop-rate").text
+        cashback_text = elem.find("div", class_="shop-rate").text
         cashback_text1 = cashback_text[cashback_text.find(":")++1:logo_text.find("%")]
         with open("cashbacker.txt", "a", encoding="utf-8") as file:
             file.write(f"{logo_text} ---->>>> {cashback_text}\n")
