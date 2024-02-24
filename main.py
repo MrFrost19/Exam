d49dce08c9b9e5640e51cdae01557bdbcdb1294a
Roman <123@gmail.com>
2024-02-24 12:39:54 +0200

    exam ver. 1.2

--- main.py
+++ main.py
@@ -5,20 +5,25 @@ from bs4 import BeautifulSoup
 session = requests.Session()
 header = {"user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Mobile Safari/537.36"}
 
-for page in range(1, 7):
-    url = f"https://cash-backer.club/shops?page={page}"
+class Exam:
 
-    response = session.get(url, headers=header)
-    soup = BeautifulSoup(response.text, "lxml")
+    def page_checker_txt_printer(self):
+        for page in range(1, 7):
+            self.url = f"https://cash-backer.club/shops?page={page}"
 
-    all_shop = soup.find("div", class_="row col-lg-9 col-md-9 col-12")
-    shop = all_shop.find_all("div", class_="col-lg-2 col-md-3 shop-list-card pseudo-link no-link")
-    shop_body = shop.find_all("div", class_="card-body")
+            self.response = session.get(self.url, headers=header)
+            self.soup = BeautifulSoup(self.response.text, "lxml")
 
-    for elem in all_shop:
-        logo_text = elem.find("div", class_="shop-title").text
-        logo_text1 = logo_text[logo_text.find(":")+1:logo_text.find("%")]
-        cashback_text = elem.find("div", class_="shop-rate").text
-        cashback_text1 = cashback_text[cashback_text.find(":")++1:logo_text.find("%")]
-        with open("cashbacker.txt", "a", encoding="utf-8") as file:
-            file.write(f"{logo_text} ---->>>> {cashback_text}\n")
+            self.all_shop = self.soup.find("div", class_="row col-lg-9 col-md-9 col-12")
+            self.shop = self.all_shop.find_all("div", class_="col-lg-2 col-md-3 shop-list-card pseudo-link no-link")
+
+            for elem in self.shop:
+                logo_text = elem.find("div", class_="shop-title").text
+                logo_text1 = logo_text[logo_text.find(":")+1:logo_text.find("%")]
+                cashback_text = elem.find("div", class_="shop-rate").text
+                cashback_text1 = cashback_text[cashback_text.find(":")++1:logo_text.find("%")]
+                with open("cashbacker.txt", "a", encoding="utf-8") as file:
+                    file.write(f"{logo_text} ---->>>> {cashback_text} of cashback\n")
+
+cashbacker = Exam()
+cashbacker.page_checker_txt_printer()
